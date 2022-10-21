f69134e06f0044ce49ed996a22552fdc0143dd60
metacogpe <metacogpe@kakao.com>
2022-10-22 08:34:13 +0900

    package entity 정의 : pk, 생성전략(db에 위임 전략)

--- src/main/java/com/pass/passbatch/repository/packaze/PackageEntity.java
+++ src/main/java/com/pass/passbatch/repository/packaze/PackageEntity.java
@@ -1,5 +1,24 @@
 package com.pass.passbatch.repository.packaze;
 
-public class PackageEntity {
+import com.pass.passbatch.repository.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity   // package 를 엔터티로 선언, 아래에서 BaseEntity 를 상속 받음
+@Table(name = "package")
+public class PackageEntity extends BaseEntity {
+    @Id  //pk 정의
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // pk 값을 위한 자동 생성 전략 : DB에 위임 설정
+    private Integer packageSeq;
+
+    private String packageName;
+    private Integer count;
+    private Integer period;
 
 }
